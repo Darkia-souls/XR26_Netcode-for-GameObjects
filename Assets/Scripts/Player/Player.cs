7a8cba7c4a9d6eff8cf6f968a0a95d628d3bb320
Darkia-souls <daria.matveeva@hyperisland.se>
2025-08-18 13:05:03 +0200

    Added Jump function to Player script

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 using Unity.Netcode;
-using Unity.Collections;
+using UnityEngine.InputSystem;
+using Unity.Netcode.Components;
+using Unity.Collections; 
 
 public class Player : NetworkBehaviour
 {
 
     [SerializeField] private PlayerChat playerChat;
+	public bool isGrounded = true;
+	public float jumpForce = 2.0f;
+	
+	public float groundDistanceCheck = 0.1f;
+
+	public LayerMask groundMask;
+
+	[SerializeField] private Rigidbody rigidBody;
+
 
 	public NetworkVariable<float> moveSpeed = 
 		new NetworkVariable<float>(
@@ -21,6 +32,11 @@ public class Player : NetworkBehaviour
             NetworkVariableReadPermission.Everyone,
             NetworkVariableWritePermission.Owner
         );
+
+	private void Awake()
+	{
+		groundMask = LayerMask.GetMask("Ground");
+	}
     
     public override void OnNetworkSpawn()
     {
@@ -54,12 +70,53 @@ public class Player : NetworkBehaviour
             Input.GetAxis("Vertical")
         );
 
+		//If the space key is pressed and the player is grounded
+		if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+		{
+			Debug.Log("CLIENT: I want to jump!");
+			JumpServerRpc();
+		}
 
         Vector3 move = input * moveSpeed.Value * Time.deltaTime;
 
         // Send the movement to the server
         MoveServerRpc(move);
-    }
+    	}
+
+		//A server function that calls the jump function to preform jump logic
+		[ServerRpc]
+		private void JumpServerRpc()
+		{
+			Debug.Log("SERVER: The player wants to jump");
+			//Actually Jump
+			Jump();
+		}
+	
+		//Apply velocity to the rigidbody to jump (works on either client or server)
+		private void Jump()
+		{
+			var vel = rigidBody.linearVelocity;
+			vel.y = 0f;
+			vel.y += jumpForce; 
+			//Because we have a *NetworkRigidbody* on the Player Prefab
+			//The linearVeloctiy will be replicated (shared) to all connected clients
+			rigidBody.linearVelocity = vel;
+		}
+
+	bool IsGrounded()
+	{
+		Vector3 origin = transform.position + Vector3.up * 0.05f;
+		float radius = 0.25f;
+		//Preform a SphereCast from the origin and the radius provided, only return results
+		//that hits the ground layermask
+		return Physics.SphereCast(origin, 
+								radius, 
+								Vector3.down, 
+								out _, 
+								10, 
+								groundMask, 
+								QueryTriggerInteraction.Ignore); 
+	}
 
     [ServerRpc]
     private void MoveServerRpc(Vector3 move, ServerRpcParams rpcParams = default)
